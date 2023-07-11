6f8b8701a143ddecf1ea75b1196673ed4d9273d6
Leandro-Pinho <leandroessencia@gmail.com>
2023-07-11 11:06:08 -0300

    create header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const Header = () => {
+  return (
+    <div className='container'>
+      <h1>Controle Financeiro</h1>
+    </div>
+  )
+}
+
+export default Header
